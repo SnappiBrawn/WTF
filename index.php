763f15feb048882f65707a585ab59bc20763b2b2
SnappiBrawn <creativeswaroop@gmail.com>
2023-07-12 10:19:47 +0530

    Development-12-07-23

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>WTF | Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="author" content="colorlib.com">
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,800" rel="stylesheet" />
     <link href="css/main.css" rel="stylesheet" />
   </head>
   <body>
+  <?php include 'utils/styles.php' ?>
+  <?php include 'utils/header.php' ?>
     <div class="s006">
       <form>
         <fieldset>
-          <legend>What are you looking for?</legend>
+          <legend>What do you got in your pantry?</legend>
           <div class="inner-form">
             <div class="input-field">
               <button class="btn-search" type="button">
@@ -19,18 +20,11 @@
                   <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
                 </svg>
               </button>
-              <input id="search" type="text" placeholder="" value="Ladies" />
+              <input id="search" type="text" placeholder="" />
             </div>
-          </div>
-          <div class="suggestion-wrap">
-            <span>New Arrivals</span>
-            <span>Ladies</span>
-            <span>Mens</span>
-            <span>Accessories</span>
-            <span>Sale</span>
           </div>
         </fieldset>
       </form>
     </div>
-  </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+  </body>
 </html>
--- utils/header.php
+++ utils/header.php
@@ -1,45 +1,94 @@
-<?php
-?>
-
 <style>
-    .navbar{
-        background-image: linear-gradient(145deg, brown, black);
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+.navbar-scroll .nav-link, .navbar-scroll .navbar-toggler-icon, .navbar-scroll .navbar-brand {
+  color: #fff;
+}
 
-    .nav-logo{
-        height: 100px;
-        width: 100px; 
-        position:relative
-        left: ; 
-    }
+.navbar-scrolled .nav-link,
+.navbar-scrolled .navbar-toggler-icon,
+.navbar-scrolled .navbar-brand {
+  color: #fff;
+}
 
-    .nav-options{
-        display: flex;
-        gap: 20px;
-    }
+.navbar-scroll,
+.navbar-scrolled {
+  background-color: #cbbcb1;
+}
 
-    .nav-options > a{
-        font-family: Impact, 'Arial Narrow Bold', sans-serif;
-        font-size: 2em;
-        color:azure;
-        
-        cursor: pointer;
-    }
-    </style>
+.mask-custom {
+  backdrop-filter: blur(0px);
+  background-color: rgba(255, 255, 255, .15);
+}
 
-<header class="navbar">
-    <div class="nav-logo">
-        <img src="images/logo.jpg" alt="logo GON!" style="height:inherit; width: inherit">
+.navbar-brand {
+  font-size: 1.75rem;
+  letter-spacing: 3px;
+}
+</style>
+<header>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+    <div class="container">
+      <a class="navbar-brand" href="#!"><span style="color: #5e9693;">Psycho</span><span style="color: #fff;">logist</span></a>
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Offer</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Reference</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#!">Contact</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav d-flex flex-row">
+          <li class="nav-item me-3 me-lg-0">
+            <a class="nav-link" href="#!">
+              <i class="fas fa-shopping-cart"></i>
+            </a>
+          </li>
+          <li class="nav-item me-3 me-lg-0">
+            <a class="nav-link" href="#!">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li class="nav-item me-3 me-lg-0">
+            <a class="nav-link" href="#!">
+              <i class="fab fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-    <nav class="nav-options">
-        <a> Find Food </a>
-        <a> Recipe Book </a>
-        <a>  </a>
-        <a> My Recipes </a>
-        <a> Login </a>
-    </nav>
-    
+  </nav>
+  <!-- Navbar -->
+
+  <!--Section: Design Block-->
+  <section>
+    <!-- Intro -->
+    <div id="intro" class="bg-image vh-100" style="
+          background-image: url('https://mdbootstrap.com/img/Photos/new-templates/psychologist/img1.jpg');
+        ">
+      
+    </div>
+    <!-- Intro -->
+  </section>
+  <!--Section: Design Block-->
 </header>
