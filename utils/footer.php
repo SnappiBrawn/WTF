277fd4eb7bb51db84e3f76b4ed89f1a3d8b11dbc
SnappiBrawn <creativeswaroop@gmail.com>
2023-07-19 22:10:58 +0530

    fixed up the footer

--- utils/footer.php
+++ utils/footer.php
@@ -2,22 +2,21 @@
 <footer>
     <div class = "container">
         <div class = "row">
-            <section>
-                <div class = "footerContent col-md-6"><!--left content-->
+            <section class="col-lg-7">
+                <div class = "footerContent"><!--left content-->
                     <p class = "footerContent1" style="font-family:monospace">
                         <strong>A</A></strong><span class = "small footerSubtext">dvancing through</span><br>
                         <strong>I</strong><span class = "small footerSubtext">nnovation and</span><br>
                         <strong>M</strong><span class = "small footerSubtext">odern</span><br>
                         <strong>S</strong><span class = "small footerSubtext">olutions</span>
                     </p>
-
                     <p class = "footerSubtext2" style="font-family:monospace">
                     IPR 20KSKB7022
                     </p>
                 </div>
             </section>
-            <section>
-                <div class = "footcontent col-md-6" ><!--middle content-->
+            <section class="col-lg-5">
+                <div class = "footcontent" ><!--middle content-->
                 "Copyright © 2023 Bangalore University. All rights reserved. No part of this website may be reproduced, distributed, or transmitted in any form, without the prior written permission of the owner, except for certain noncommercial uses permitted by copyright law. For permission requests, write to the publisher at kswaroophere@gmail.com."
                 </div>
             </section>
